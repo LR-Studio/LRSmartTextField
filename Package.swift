52307ed9792b44739f6bd9502103162cf00dc81e
Willian Honda <willian_honda@apple.com>
2022-07-12 20:56:46 -0300

    Updated Package with Objective-C

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "LRSmartTextField",
+    name: "LRTextField",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "LRSmartTextField",
-            targets: ["LRSmartTextField"]),
+            name: "LRTextField",
+            targets: ["LRTextField"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,10 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "LRSmartTextField",
+            name: "LRTextField",
             dependencies: []),
-        .testTarget(
-            name: "LRSmartTextFieldTests",
-            dependencies: ["LRSmartTextField"]),
     ]
 )
